0038cfa311dff1d6cba60304cc9a0aa7349d65d9
Tomer Shafir <tomershafir@Tomers-MacBook-Pro-5.local>
2022-07-06 13:26:53 +0300

    add transactions execution

--- Blockchain.py
+++ Blockchain.py
@@ -2,7 +2,7 @@ from typing import List
 from Block import Block
 from BlockchainUtils import BlockchainUtils
 from Transaction import Transaction
-from constants import LAST_INDEX
+from constants import LAST_INDEX, TRANSFER_OPERATIONS
 from AccountModel import AccountModel
 from pprint import pprint
 
@@ -16,9 +16,10 @@ class Blockchain():
         self.account_model = AccountModel()
         
     def add_block(self, block: Block) -> None:
+        self.execute_transactions(block.transactions)
         self.blocks.append(block)
         
-    def to_json(self):
+    def to_json(self) -> dict:
         data = {}
         json_blocks = []
         for block in self.blocks:
@@ -31,26 +32,42 @@ class Blockchain():
             return True
         return False
     
-    def last_block_hash_validity(self, block:Block):
+    def last_block_hash_validity(self, block:Block) -> bool:
         latest_blockchain_block_hash = BlockchainUtils.hash(self.blocks[LAST_INDEX].payload()).hexdigest()
         if latest_blockchain_block_hash == block.last_hash:
             return True
         return False
     
-    def transaction_cover(self, transaction: Transaction):
+    def transaction_covered(self, transaction: Transaction) -> bool:
+        if transaction.transaction_type == TRANSFER_OPERATIONS.EXCHANGE:
+            return True
         sender_balance = self.account_model.get_balance(transaction.sender_pub_key)
         if sender_balance >= transaction.amount:
             return True
         return False
     
-    def get_covered_transaction_set(self, transactions: List[Transaction]):
+    def get_covered_transaction_set(self, transactions: List[Transaction]) -> List[Transaction]:
         covered_transactions = []
         for transaction in transactions:
-            if self.transaction_cover(transaction):
+            if self.transaction_covered(transaction):
                 covered_transactions.append(transaction)
             else:
                 print('The following transaction is not covered by the sender!')
                 pprint(transaction.to_json())
                 
         return covered_transactions
+    
+    def execute_transaction(self, transaction: Transaction) -> None:
+        sender = transaction.sender_pub_key
+        reciever = transaction.reciever_pub_key
+        amount = transaction.amount
+        
+        self.account_model.update_balance(sender, -amount)
+        self.account_model.update_balance(reciever, amount)
+        
+    def execute_transactions(self, transactions: List[Transaction]) -> None:
+        for transaction in transactions:
+            self.execute_transaction(transaction)
+            
+        
             
--- Main.py
+++ Main.py
@@ -18,12 +18,31 @@ if __name__ == '__main__':
     
     alice = Wallet()
     bob = Wallet()
+    exchange = Wallet()
+    forger = Wallet()
+    
+    exchange_transaction = exchange.create_transaction(alice.get_public_key_string(), 10,
+                                                       TRANSFER_OPERATIONS.EXCHANGE)
+    
+    if not pool.is_transaction_exists(exchange_transaction):
+        pool.add_transaction(exchange_transaction)
+    
+    covered_transactions = blockchain.get_covered_transaction_set(pool.transactions)
+    block_one = Block(covered_transactions,
+                      BlockchainUtils.hash(blockchain.blocks[LAST_INDEX].payload()).hexdigest(),
+                      forger.get_public_key_string(),blockchain.blocks[LAST_INDEX].block_count + 1)
+    blockchain.add_block(block_one)
     
     transaction = alice.create_transaction(bob.get_public_key_string(),5,
                                            TRANSFER_OPERATIONS.TRANSFER)
     if not pool.is_transaction_exists(transaction):
         pool.add_transaction(transaction)
+
+    covered_transactions = blockchain.get_covered_transaction_set(pool.transactions)
+    block_two = Block(covered_transactions,
+                      BlockchainUtils.hash(blockchain.blocks[LAST_INDEX].payload()).hexdigest(),
+                      forger.get_public_key_string(),blockchain.blocks[LAST_INDEX].block_count + 1)
+    blockchain.add_block(block_two)     
     
-    covered_transaction = blockchain.get_covered_transaction_set(pool.transactions)
-    print(covered_transaction)
+    pprint.pprint(blockchain.to_json())
     
